2ae187f07169c7d34481471dd640c7d05c32f3ae
mattiapasquini <mattia.pasquini@samtrevano.ch>
2021-10-28 16:23:15 +0200

    28.10.2021

--- Source/chat/script.js
+++ Source/chat/script.js
@@ -9,22 +9,44 @@ const firebaseConfig = {
   appId: "1:1017316498618:web:8d82c647cf37d8e142f683"
 };
 
-
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 
 // Initialize database
 const db = firebase.database();
 
-//#region 
+//#region metodi helper
+function checkName(name){
+  db.ref('users/').on('value', (snapshot)=>{
+    var listUsers = [];
+    snapshot.forEach((item) =>{
+      var itemVal = item.val();
+      listUsers.push(itemVal);
+    })
+    //console.log(listUsers);
+    for(element of listUsers){
+      if(element[`username`]==name){
+        console.log("name already exists");
+        return false;
+      }
+    }
+  });
+  return true;
+}
+//#endregion
+
+//#region autenticazione
+
 function registerUser(){
+    //console.log("cia");
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    //Email sarà univoco e 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
+    //i nomi devono essere univoci
+    if(checkName(name)){
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
         //Signed in
         const user = userCredential.user;
         var uid = user.uid;
@@ -35,12 +57,15 @@ function registerUser(){
             isAdmin: false
         })
         console.log(user);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorMessage);
-    });
+      });
+    }
+    //Email e nome saranno univoci
+    
 }
 
 var currentUser = null;
@@ -56,6 +81,13 @@ function loginUser(){
     db.ref('users/' + userCredential.user.uid).on("value", (snapshot)=>{
       currentUser = snapshot.val().username;
       console.log(currentUser);
+
+      if(snapshot.val().isAdmin){
+        var ad = document.getElementsByName("admin-mode");
+        for(element of ad){
+          element.style.display = "block";
+        }
+      }
     })
     var user = userCredential.user;
     console.log(user);
@@ -64,7 +96,8 @@ function loginUser(){
     document.getElementById("chat-section").style.display = "block";
     document.getElementById("login-section").style.display = "none";
     // ...
-    
+    chat();
+    showChannels();
   })
   .catch((error) => {
     var errorCode = error.code;
@@ -107,7 +140,8 @@ function sendMessage(e){
 }
 
 //se ci sono nuovi messaggi, vengono aggiunto
-console.log(currentUser);
+function chat(){
+  console.log(currentUser);
 db.ref('messages/').on("child_added", function (snapshot) {
   const messages = snapshot.val();
   const message = `<li class=${
@@ -116,4 +150,75 @@ db.ref('messages/').on("child_added", function (snapshot) {
   // append the message on the page
   document.getElementById("messages").innerHTML += message;
 });
+}
+
 //#endregion
+
+//#region Canali
+
+function channelSection(){
+  document.getElementById("list-user").innerHTML = "";
+  document.getElementById("chat-section").style.display = "none";
+  document.getElementById("createchannel-section").style.display = "block";
+  
+  db.ref('users/').on('value', (snapshot)=>{
+    var listUsers = [];
+    snapshot.forEach((item) =>{
+      var itemVal = item.val();
+      listUsers.push(itemVal);
+    })
+    //console.log(listUsers);
+    for(element of listUsers){
+      console.log(element[`username`]);
+      var aUser = `<input type="checkbox" name="user" id="${element[`username`]}" value="${element[`username`]}"><label for="${element[`username`]}">${element[`username`]}</label><br>`;
+      
+        console.log(aUser);
+      document.getElementById("list-user").innerHTML += aUser;
+    }
+  });
+  
+}
+
+function createChannel(){
+  var nameChannel = document.getElementById("name-channel").value;
+  if(nameChannel === ''){
+    return false; 
+  }
+  
+  //inserisci gli utenti selezionati nell'array
+  usersSelected = [];
+  var users = document.getElementById("list-user").children;
+  Array.from(users).forEach(input =>{
+    if(input.checked){
+      usersSelected.push(input.id);
+    }
+  });
+
+  db.ref('channels/' + nameChannel).set({
+    name: nameChannel,
+    users: usersSelected,
+  });
+
+  document.getElementById("createchannel-section").style.display = "none";
+  document.getElementById("chat-section").style.display = "block";
+}
+
+
+//da finire
+function showChannels(){
+  db.ref('channels/').on("child_added", function (snapshot) {
+    const channels = Object.entries(snapshot.val());
+    for(elements of channels){
+
+      for(user of elements.users){
+        if(currentUser == user){
+          const channel = `<li><span>${element.name}</span></li>`;
+          document.getElementById("channels").innerHTML += channel;
+        }
+      }
+    }
+  });
+}
+
+//#endregion
+
